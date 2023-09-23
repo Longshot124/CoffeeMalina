6e625de0baa180a8821c91703b461767f9ef05c8
kamra <kamra@LAPTOP-M49P7DNF>
2023-09-23 21:34:54 +0400

    Identity in Process

--- Malina/Controllers/AccountController.cs
+++ Malina/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Malina.Core.Entities;
+﻿using Malina.BLL.VIewModels.Acconts;
+using Malina.Core.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,7 @@ namespace Malina.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
@@ -20,6 +22,31 @@ namespace Malina.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var existUser = await _userManager.FindByNameAsync(model.UserName);
+
+            if(existUser == null)
+            {
+                ModelState.AddModelError("", "Invalid Credentials");
+                return View();
+            }
+
+            var signResult = await _signInManager.PasswordSignInAsync(existUser, model.Password, model.RememberMe,false);
+
+            if (!signResult.Succeeded)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
         public IActionResult Index()
         {
             return View();
